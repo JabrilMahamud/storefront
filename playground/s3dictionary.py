da438af0808aedf8915e9a453a79b4e92692159c
JabrilMahamud <Jabril.mahamud@bp.com>
2022-09-29 09:16:41 +0100

    updated s3dictionary-> outputs a dictionary

--- playground/s3dictionary.py
+++ playground/s3dictionary.py
@@ -1,17 +1,13 @@
 from modulefinder import Module
 import boto3
-from datetime import date, datetime
 
-def dictionaryCreator():
-    dynamodb = boto3.resource("dynamodb", region_name="eu-west-2")
-    table = dynamodb.Table("MetadataJson")
-    tableDict = table.scan(
-        ProjectionExpression="#AN, account, #S",
-        ExpressionAttributeNames={
-            "#AN": "account-name",
-            "#S": "status",
-        },
-    )
-    return tableDict
+dynamodb = boto3.resource("dynamodb", region_name="eu-west-2")
+table = dynamodb.Table("MetadataJson")
+tableDict = table.scan(
+    ProjectionExpression="#AN, account, #S",
+    ExpressionAttributeNames={
+        "#AN": "account-name",
+        "#S": "status",
+    },
+)
 
-print("Complete")
--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .s3dictionary import dictionaryCreator
-
+from .s3dictionary import tableDict
 # Create your views here.
 #this is the request handler
 #turns requests into responses
 
+
+####actual views part of the file
 def  say_hello(request):
     return render(request, 'hello.html',{'name':'Jabril'})
 
 def s3RequestHandler(request):
-       return render(request,'hello.html',dictionaryCreator())
+       return render(request,'jsonDisplay.html',tableDict)
     
